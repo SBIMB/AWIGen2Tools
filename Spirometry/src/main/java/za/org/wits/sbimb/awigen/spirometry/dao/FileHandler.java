02d3eda3ae50a03ba2fa3d83a01fbf9ba75da8f2
Freedom Mukomana <mukomanaf@yahoo.com>
2019-07-09 15:50:40 +0200

    Structural Changes

---
+++ Spirometry/src/main/java/za/org/wits/sbimb/awigen/spirometry/dao/FileHandler.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package za.org.wits.sbimb.awigen.spirometry.dao;
+
+/**
+ * @author A0035863
+ *
+ */
+public class FileHandler {
+
+}
---
+++ Spirometry/src/main/java/za/org/wits/sbimb/awigen/spirometry/util/Xml2CsvConverter.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package za.org.wits.sbimb.awigen.spirometry.util;
+
+/**
+ * @author Freedom Mukomana
+ *
+ */
+public class Xml2CsvConverter {
+
+}
